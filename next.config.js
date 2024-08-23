2b8375241b743e9fab499e9ad6d7aee1693f6fa5
Marcus Liljehammar <liljehammar@gmail.com>
2024-08-23 13:23:06 +0200

    added more pwa functionality

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
+  disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
 });
 
