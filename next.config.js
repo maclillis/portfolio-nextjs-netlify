c7f7b54d594c685b27a268a506a396a5341256bf
Marcus Liljehammar <liljehammar@gmail.com>
2024-08-25 13:11:20 +0200

    removed webpack analyzer

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 
 const path = require('path');
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
@@ -45,4 +41,3 @@ const nextConfig = {
 };
 
 module.exports = withPWA(nextConfig);
-module.exports = withBundleAnalyzer(nextConfig);
