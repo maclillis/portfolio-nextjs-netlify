bb71f6e4411c8713fe2c4366ad06dafbb1d9a13e
Marcus Liljehammar <liljehammar@gmail.com>
2024-08-26 10:39:20 +0200

    updated about

--- pages/om.js
+++ pages/om.js
@@ -27,19 +27,19 @@ export default function About() {
                 <BreadcrumbItem>Om</BreadcrumbItem>
             </Breadcrumbs>
 
-            <section className="py-6 lg:py-8 w-full md:px-5">
+            <section className="py-6 lg:py-8 w-full px-0 lg:px-20">
                 <h2 className="pb-4">Biografi</h2>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Jag har kodat hemsidor sedan gymnasiet och har under årens lopp vandra mellan det kreativa och det tekniska med stadiga, nyfikna steg.</p>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Under flera år frilansade jag som grafisk designer och kom i kontakt med plattformar som Wordpress och Joomla redan 2008. Här någonstans föddes också passionen för användbarhet och tillsammans med det kreativa skapa verktyg som både förtrollar och kan användas av alla, oavsett kompetens.</p>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Jag gillar verkligen människan och hennes beteende och det finns nog inget mer fascinerande än att studera det som i det digitala, på webben.</p>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Det finns något riktigt speciellt att starta en digital resa med någon, att se  något formas till en konverterande plattform för någon att få ut sitt budskap i, vare sig det gäller sälja inom e-handel till bara att publicera historier i en blogg.</p>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Mitt fokus ligger väldigt mycket i att balansera det snygga med det tekniska och lever enligt deviserna ”less is more” och ”keep it simple, stupid” eftersom enkelhet är det första steget i att skapa verktyg alla kan använda och det är där magin finns.</p>
-                <p className={`${styles.about_text} lg:max-w-2xl `}>Ska vi göra något kul tillsammans? <Link ahref="/kontakt">Kontakta mig gärna via formuläret här</Link> eller <Link ahref="https://www.linkedin.com/in/marcusliljehammar/" target="_blank">på LinkedIn</Link>, så tar vi därifrån.</p>
+                <p className={`${styles.about_text_start}`}>Jag har kodat hemsidor sedan gymnasiet och har under årens lopp vandra mellan det kreativa och det tekniska med stadiga, nyfikna steg.</p>
+                <p className={`${styles.about_text}`}>Under flera år frilansade jag som grafisk designer och kom i kontakt med plattformar som Wordpress och Joomla redan 2008. Här någonstans föddes också passionen för användbarhet och tillsammans med det kreativa skapa verktyg som både förtrollar och kan användas av alla, oavsett kompetens.</p>
+                <p className={`${styles.about_text}`}>Jag gillar verkligen människan och hennes beteende och det finns nog inget mer fascinerande än att studera det som i det digitala, på webben.</p>
+                <p className={`${styles.about_text}`}>Det finns något riktigt speciellt att starta en digital resa med någon, att se  något formas till en konverterande plattform för någon att få ut sitt budskap i, vare sig det gäller sälja inom e-handel till bara att publicera historier i en blogg.</p>
+                <p className={`${styles.about_text}`}>Mitt fokus ligger väldigt mycket i att balansera det snygga med det tekniska och lever enligt deviserna ”less is more” och ”keep it simple, stupid” eftersom enkelhet är det första steget i att skapa verktyg alla kan använda och det är där magin finns.</p>
+                <p className={`${styles.about_text}`}>Ska vi göra något kul tillsammans? <Link ahref="/kontakt">Kontakta mig gärna via formuläret här</Link> eller <Link ahref="https://www.linkedin.com/in/marcusliljehammar/" target="_blank">på LinkedIn</Link>, så tar vi det därifrån.</p>
             </section>
 
-            <section className="py-6 lg:py-8 w-full md:px-5">
+            <section className="py-6 lg:py-8  w-full px-0 lg:px-20">
                 <h2 className="pb-7">Erfarenhet</h2>
-                <div className="grid grid-cols-3 gap-3 md:gap-6 pb-8">
+                <div className="grid grid-cols-3 gap-3 md:gap-6 py-6">
                     <div className="col-span-1 text-center flex flex-col items-center">
                         <ShoppingBagIcon className={`${styles.icon_grid} stroke-1`} />
                         <h3 className={`${styles.experience_label} text-base lg:text-xl pt-2`}>E-handel</h3>
@@ -71,41 +71,41 @@ export default function About() {
                 <p className={`${styles.about_text} py-0 md:py-8`}>Och även: Varumärkesutveckling & Marknadsföring, Grafisk design och Google Analytics/Tag Manager.</p>
             </section>
 
-            <section className="py-6 lg:py-8 w-full md:px-5">
+            <section className="py-6 lg:py-8 w-full px-0 lg:px-20">
                 <h2 className="pb-4">Skills</h2>
                 <div className="grid grid-cols-3 gap-y-6">
                     <div className="col-span-2">
                         <h3 className={styles.skills_label}>Ramverk</h3>
-                        <p className={styles.skills_models}>React, Vue (Next.js, Nuxt.js</p>
+                        <p className={styles.skills_models}>React, Vue (Next.js, Nuxt.js)</p>
                     </div>
                     <div className="col-span-1 flex justify-center items-center">
-                        <Chip className="text-white" size="md" variant="bordered">MEDELNIVÅ</Chip>
+                        <Chip className="text-white" size="md" variant="bordered">+5 ÅRS ERFARENHET</Chip>
                     </div>
                     <div className="col-span-2">
                         <h3 className={styles.skills_label}>CMS</h3>
                         <p className={styles.skills_models}>Wordpress, Sanity & Strapi</p>
                     </div>
                     <div className="col-span-1 flex justify-center items-center">
-                        <Chip className="text-white" size="md" variant="bordered" >MEDELNIVÅ</Chip>
+                        <Chip className="text-white" size="md" variant="bordered" >+8 ÅRS ERFARENHET</Chip>
                     </div>
                     <div className="col-span-2">
                         <h3 className={styles.skills_label}>Frontend</h3>
                         <p className={styles.skills_models}>HTML, CSS, JavaScript, TypeScript, JSON</p>
                     </div>
                     <div className="col-span-1 flex justify-center items-center">
-                        <Chip className="text-white" size="md" variant="bordered">MEDELNIVÅ</Chip>
+                        <Chip className="text-white" size="md" variant="bordered">+8 ÅRS ERFARENHET</Chip>
                     </div>
                     <div className="col-span-2">
                         <h3 className={styles.skills_label}>API</h3>
                         <p className={styles.skills_models}>REST, graphQL, Webhooks</p>
                     </div>
                     <div className="col-span-1 flex justify-center items-center text-white">
-                        <Chip className="text-white" size="md" variant="bordered">MEDELNIVÅ</Chip>
+                        <Chip className="text-white" size="md" variant="bordered">+3 ÅRS ERFARENHET</Chip>
                     </div>
                 </div>
             </section>
 
-            <section className="py-6 lg:py-8 w-full md:px-5">
+            <section className="py-6 lg:py-8 w-full px-0 lg:px-20">
                 <h2 className="pb-4">Kodlogistik</h2>
                 <p className={styles.about_text}>Versionshantering med GIT och Github och kunskap om CI/CD-utveckling</p>
 
