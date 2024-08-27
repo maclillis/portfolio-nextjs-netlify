4ebc5056749c54808f52c959225953352a8437de
Marcus Liljehammar <liljehammar@gmail.com>
2024-08-27 11:17:11 +0200

    added proper form and slack integrations

--- components/modules/ContactModule.js
+++ components/modules/ContactModule.js
@@ -1,6 +1,7 @@
 import {Input, Button, Select, SelectItem, Textarea} from "@nextui-org/react";
 
 import React, { useState } from 'react';
+import {CircularProgress} from "@nextui-org/react";
 
 import styles from './ContactModule.module.scss'
 
@@ -21,117 +22,108 @@ export default function ContactModule() {
     }
   };
 
-    const [formData, setFormData] = useState({
-        namn: '',
-        email: '',
-        meddelande: ''
-      });
-      const [loading, setLoading] = useState(false);
-      const [error, setError] = useState('');
-      const [success, setSuccess] = useState('');
+  const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      message: '',
+      subject: '',
+    });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [responseMessage, setResponseMessage] = useState(false);
     
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-
-        const newFormData = {
-              ...formData,
-              [name]: value, 
-            };
-
-        setFormData(newFormData);
-      };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
     
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setError('');
-        setSuccess('');
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsSubmitting(true);
 
-        event.preventDefault();
-
-        const formData = {
-          name: event.target.name.value,
-          email: event.target.email.value,
-          message: event.target.message.value,
-          subject: event.target.subject.value,
-        };
-
-        //Netlify compatibility AJAX-call
-        /*const formData = new FormData(event.target);
-          await fetch('/__forms.html', {
-              method: 'POST',
-              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-              body: new URLSearchParams(formData).toString()
-        });*/
-
-        const res = await fetch('/api/contact', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-        });
-
-        const result = await res.json();
-  
-        if (res.ok) {
-          setSuccess('Meddelandet skickats!');
-          console.log('success! ' + result);
-        } else {
-          setError('Misslyckades med att skicka meddelandet');
-          console.log('fail!');
-        }
-
-        if (!window.grecaptcha) {
-          console.error('reCAPTCHA not yet loaded');
-          return;
-        }
-    
-        const token = await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: 'submit' });
-        setRecaptchaToken(token);
-    
-        const response = await fetch('/api/verifyRecaptcha', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ recaptchaToken: token }),
-        });
-    
-        const data = await response.json();
-    
-        if (data.success) {
-          // Continue with form submission
-        } else {
-          console.log("reCAPTCHA failed");
-        }
-
+    if (!window.grecaptcha) {
+      console.error('reCAPTCHA not yet loaded');
+      return;
     }
+    try {
+
+    const recaptchaToken = await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: 'submit' });
+    setRecaptchaToken(token);
+
+    const response = await fetch('/api/verifyRecaptcha', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        ...formData,
+        recaptchaToken}),
+    });
+
+    const data = await response.json();
+
+    if (data.ok) {
+      // Continue with form submission
+    } else {
+      console.log("reCAPTCHA failed");
+    }
+
+  } catch (error) {
+      setResponseMessage('Failed to send message. Please try again later.');
+    } finally {
+      setIsSubmitting(false);
+  }
+
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setResponseMessage(true);
+      } else {
+        setResponseMessage(`Error: ${result.error}`);
+      }
+    } catch (error) {
+      setResponseMessage('Failed to send message. Please try again later.');
+    } finally {
+      setIsSubmitting(false);
+    }
+
+}
     
     return(
-        <form id="contact_form" className={`${styles.contact_form_wrap} grid grid-cols-1 py-5 md:grid-cols-2 md:gap-x-8 w-full`} netlify="true" onSubmit={handleSubmit}>
+        <form id="contact_form" className={`${styles.contact_form_wrap} grid grid-cols-1 py-5 md:grid-cols-2 md:gap-x-8 w-full`} onSubmit={handleSubmit}>
             <div className="w-full">
 
             <input type="text" name="honey" style={{ display: 'none' }} onChange={handleChange} />
 
-            <Input onFocus={loadRecaptcha} value={formData.namn} id="name" labelPlaceholder="Namn" type="text" name="namn" label="Namn" labelPlacement="inside" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[focus=true]:text-gray-400"], inputWrapper: ["contact_form_bg", "focus-within:!border-white"]}} className="py-2" onChange={handleChange} required />
+            <Input onFocus={loadRecaptcha} value={formData.namn} id="name" labelPlaceholder="Namn" type="text" name="name" label="Namn" labelPlacement="inside" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[focus=true]:text-gray-400"], inputWrapper: ["contact_form_bg", "focus-within:!border-white"]}} className="py-2" onChange={handleChange} required />
 
             <Input value={formData.email} id="email" type="email" label="Email" name="email" labelPlacement="inside" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[focus=true]:text-gray-400"], inputWrapper: ["contact_form_bg", "focus-within:!border-white"]}} className="py-2" onChange={handleChange} required />
 
-            <Select id="subject" labelPlacement="inside" name="ärende" label="Ärende" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[filled=true]:text-gray-400"], inputWrapper: "focus-within:!border-white", value: "text-white",  trigger: ["contact_form_select_bg", "group-data-[filled=true]:border-white"], popoverContent: ["select_popout_bg", "focus-within:!border-white"], listbox: "group-data-[focus=true]:text-gray-400" }} className="py-2" onChange={handleChange} required>
+            <Select value={formData.subject} id="subject" labelPlacement="inside" name="subject" label="Ärende" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[filled=true]:text-gray-400"], inputWrapper: "focus-within:!border-white", value: "text-white",  trigger: ["contact_form_select_bg", "group-data-[filled=true]:border-white"], popoverContent: ["select_popout_bg", "focus-within:!border-white"], listbox: "group-data-[focus=true]:text-gray-400" }} className="py-2" onChange={handleChange} required>
                 <SelectItem classNames={{base: "data-[selectable=true]:focus:bg-purple-200"}} key="Samarbete" value="Samarbete">Samarbete</SelectItem>
                 <SelectItem classNames={{base: "data-[selectable=true]:focus:bg-purple-200"}} key="Rekrytering" value="Rekrytering">Rekrytering</SelectItem>
                 <SelectItem classNames={{base: "data-[selectable=true]:focus:bg-purple-200"}} key="Övrigt" value="Övrigt">Övrigt</SelectItem>
             </Select>
             </div>
             <div className="w-full md:h-full">
-                <Textarea value={formData.meddelande} id="message" type="text" name="meddelande" label="Meddelande" labelPlacement="inside" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[focus=true]:text-gray-400"], inputWrapper: ["contact_form_bg", "focus-within:!border-white", "textarea", "text-base"]}}  className="py-2 h-full text-base" onChange={handleChange} required />
+                <Textarea value={formData.meddelande} id="message" type="text" name="message" label="Meddelande" labelPlacement="inside" variant="bordered" classNames={{label: ["contact_form_label", "group-data-[focus=true]:text-gray-400"], inputWrapper: ["contact_form_bg", "focus-within:!border-white", "textarea", "text-base"]}}  className="py-2 h-full text-base" onChange={handleChange} required />
             </div>
 
             <div className="flex pt-5 col-span-1 md:col-start-2 w-full md:justify-end">
-                <Button type="submit" className="button_base button_primary btn_internal py-2 px-4 w-full">{loading ? 'Skickar...' : 'Skicka meddelande'} </Button>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                {success && <p style={{ color: 'green' }}>{success}</p>}
+                <Button type="submit" className="button_base button_primary btn_internal py-2 px-4 w-full">
+                {isSubmitting ? ( <> Skickar...  <CircularProgress size="sm" color="default" /> </>) : ('Skicka')}
+                </Button>
+                {responseMessage && <p>{responseMessage}</p> }
             </div>
         </form>
     )
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,34 +1,79 @@
 import nodemailer from 'nodemailer';
+import axios from 'axios';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { name, email, message, subject } = req.body;
+    const { name, email, message, subject, recaptchaToken } = req.body;
+
+    // Verify reCAPTCHA
+    const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY;
+    const recaptchaResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `secret=${recaptchaSecret}&response=${recaptchaToken}`,
+    });
+
+    const recaptchaResult = await recaptchaResponse.json();
+
+    if (!recaptchaResult.success || recaptchaResult.score < 0.5) {
+      
+      const slackMessage = {
+          text: `:x: *reCAPTCHa-validering misslyckades för kontaktformuläret!* :warning:\n\n\n*Namn:* ${name}\n*Email:* ${email}\n*Meddelande:*\n\n${message}`
+      };
+
+      await fetch(process.env.SLACK_WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(slackMessage),
+      });
+
+      return res.status(400).json({ error: 'Valideringen misslyckades. Försök igen.' });
+    }
 
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_SERVER,
-      port: 465, // or 465 for secure
-      secure: true, // true if port is 465
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
-
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_TO_MAIL,
-      subject: `Nytt meddelande från ${name} - ${subject}`,
-      text: subject + '<br>'+ message + '<br>Email: ' + email,
-    };
-
     try {
+      const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_TO_MAIL,
+        subject: `Nytt meddelande från ${name} - ${subject}`,
+        text: `${message}
+              Email: ${email} 
+              `,
+        html: `<p>${message.replace(/\n/g, '<br>')}</p><p><strong> Email:</strong> ${email}</p>`,
+      };
+  
+      //Let's send a notification to Slack
+      const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+      await axios.post(slackWebhookUrl, {
+          text: `*Nytt meddelande från kontaktformuläret!* :memo:`
+      });
+
       await transporter.sendMail(mailOptions);
       res.status(200).json({ success: true });
     } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ error: 'Error sending email' });
+      console.error('Form submission failed:', error);
+
+      // Send Slack error notification
+      const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+      await axios.post(slackWebhookUrl, {
+          text: `:x: *Kontaktformuläret skickade inte!* :warning:\n\n*Error:* ${error.message}\n\n*Namn:* ${name}\n*Email:* ${email}\n*Meddelande:*\n\n${message}`
+      });
+      res.status(500).json({ error: 'Failed to submit the form. Please try again later.' });
     }
   } else {
+    res.setHeader('Allow', ['POST']);
     res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
