6e56c73ab95edc70386a21e97d9a2a4fddb98685
Marcus Liljehammar <liljehammar@gmail.com>
2024-08-28 15:00:32 +0200

    added og meta imgs width height

--- pages/index.js
+++ pages/index.js
@@ -79,6 +79,8 @@ export default function Home({blogPosts, workPosts}) {
           <meta property="og:title" content="Marcus Liljehammar - Webutvecklare & UX/Digital Designer" />
           <meta property="og:description" content="Jag har 12+ års erfarenhet att jobba professionellt med framställandet av vackra, enkla att använda och snabba webbappar och hemsidor som konverterar." />
           <meta property="og:image" content="https://marcusliljehammar.se/assets/images/some_marcus_banner.jpg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
           <meta property="og:url" content={`${baseURL}`} />
           <meta name="author" content="Marcus Liljehammar" />
 
