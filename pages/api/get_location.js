144a63ca323df174d2757436eba5b9e2c6bf717a
Marcus Liljehammar <liljehammar@gmail.com>
2024-09-02 15:25:27 +0200

    uam api update

--- pages/api/get_location.js
+++ pages/api/get_location.js
@@ -1,10 +1,30 @@
 export default async function handler(req, res) {
-    const response = await fetch(`https://ipinfo.io/json?token=${process.env.IPINFO_API_TOKEN}`);
-    const data = await response.json();
+    const token = process.env.IPINFO_API_TOKEN;
   
-    res.status(200).json({
-      city: data.city,
-      region: data.region,
-      country: data.country,
-    });
+    if (!token) {
+      console.error('IPINFO_API_TOKEN is not set');
+      return res.status(500).json({ error: 'API token is missing' });
+    }
+  
+    console.log('Using IPINFO_API_TOKEN:', token);
+  
+    try {
+      const response = await fetch(`https://ipinfo.io/json?token=${token}`);
+      const data = await response.json();
+  
+      if (response.ok) {
+        res.status(200).json({
+          city: data.city,
+          region: data.region,
+          country: data.country,
+          ip: data.ip,
+        });
+      } else {
+        console.error('Error fetching location data:', data);
+        res.status(response.status).json({ error: 'Error fetching location data' });
+      }
+    } catch (error) {
+      console.error('Error during fetch:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
